5f9d241c8850906f89b95982677b7b07a7352fec
JoeLiu1321 <angrybirdliu@gmail.com>
2019-07-16 17:29:17 +0800

    finish add-member-page

--- src/app/add-member-page/add-member-page.component.ts
+++ src/app/add-member-page/add-member-page.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormBuilder,FormGroup,Validators,FormControl} from '@angular/forms';
 @Component({
   selector: 'app-add-member-page',
   templateUrl: './add-member-page.component.html',
   styleUrls: ['./add-member-page.component.css']
 })
-export class AddMemberPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class AddMemberPageComponent{
+  memberForm:FormGroup;
+  name; 
+  identify;
+  phone; 
+  address;
+  email;
+  password;
+  confirmPassword;
+  passwordEqual=false;
+  constructor(private formBuilder:FormBuilder){ 
+    this.memberForm=this.formBuilder.group({
+      name:['',Validators.minLength(3)],
+      identify:'',
+      phone:'',
+      address:'',
+      email:['',Validators.email],
+      password:'',
+      confirmPassword:'',
+    });
+    this.name = this.memberForm.controls['name'];
+    this.identify = this.memberForm.controls['identify'];
+    this.phone = this.memberForm.controls['phone'];
+    this.address = this.memberForm.controls['address'];
+    this.email = this.memberForm.controls['email'];
+    this.password = this.memberForm.controls['password'];
+    this.confirmPassword = this.memberForm.controls['confirmPassword'];
+  
   }
+
+  validPassword(){
+    return(this.password.value==this.confirmPassword.value); 
+  }
+  submit(formData){
+    for(let data in formData)
+      console.log(`${data} :`+formData[data])
+   
+  }
+
 
 }
